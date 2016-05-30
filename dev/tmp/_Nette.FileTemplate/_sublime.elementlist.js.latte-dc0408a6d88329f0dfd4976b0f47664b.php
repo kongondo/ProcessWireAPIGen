9ac4d9a7aee1bbdaa9ba1a2e43e69b0ef26997ec
Francis Otieno <kongondo@gmail.com>
2016-05-30 10:00:09 +0100

    Latest changes

---
+++ dev/tmp/_Nette.FileTemplate/_sublime.elementlist.js.latte-dc0408a6d88329f0dfd4976b0f47664b.php
@@ -0,0 +1,21 @@
+<?php //netteCache[01]000376a:2:{s:4:"time";s:21:"0.87722600 1464596164";s:9:"callbacks";a:2:{i:0;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:9:"checkFile";}i:1;s:62:"F:\CMS\pear\data\ApiGen\templates\sublime\elementlist.js.latte";i:2;i:1391390098;}i:1;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:10:"checkConst";}i:1;s:25:"Nette\Framework::REVISION";i:2;s:22:"released on 2013-12-31";}}}?><?php
+
+// source file: F:\CMS\pear\data\ApiGen\templates\sublime\elementlist.js.latte
+
+?><?php
+// prolog Nette\Latte\Macros\CoreMacros
+list($_l, $_g) = Nette\Latte\Macros\CoreMacros::initRuntime($template, 'nua9uptj2n')
+;
+// prolog Nette\Latte\Macros\UIMacros
+
+// snippets support
+if (!empty($_control->snippetMode)) {
+	return Nette\Latte\Macros\UIMacros::renderSnippets($_control, $_l, get_defined_vars());
+}
+
+//
+// main template
+// ?>
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = <?php echo Nette\Templating\Helpers::escapeJs($elements) ?>;
